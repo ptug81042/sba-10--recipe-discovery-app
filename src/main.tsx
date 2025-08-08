87741333f17dfc78dc83034d3ed3a9ee4e8eedca
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-08-08 11:22:47 -0400

    chore(main): wrap App with FavoritesProvider for global favorites state
    - Imported and wrapped app component tree inside FavoritesProvider
    - Ensures favorites context is available throughout application
    - Maintains previous ThemeProvider and BrowserRouter wrappers intact

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { FavoritesProvider } from './contexts/FavoritesContext.tsx'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <FavoritesProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </FavoritesProvider>
     </ThemeProvider>
   </StrictMode>,
 )
